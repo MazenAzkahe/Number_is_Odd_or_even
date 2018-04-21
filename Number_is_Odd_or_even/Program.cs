713b9f232a9417d209e4bc238c3478d0f2f089c0
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-21 12:12:31 -0700

    Add project files.

---
+++ Number_is_Odd_or_even/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Number_is_Odd_or_even
+{
+    class Program
+    {
+        enum Num { odd ,even}
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+                Console.WriteLine("Enter The Number");
+                double num = double.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"The Number is {Num.even}");
+                }
+                else
+                {
+                    Console.WriteLine($"The Number is {Num.odd} ");
+                }
+                Console.WriteLine("Try Agin ? Y/N ");
+                str = Console.ReadLine();
+            } while (str.Trim().ToLower() == "y");
+          
+       
+        }
+    }
+}
